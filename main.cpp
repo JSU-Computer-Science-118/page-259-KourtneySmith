36290177a52af41cbf59de0f67111145fe0150f8
Kourtney Smith <117469291+KourtneySmith@users.noreply.github.com>
2022-11-11 20:43:43 -0600

    Change_0

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int a, b, c;
-int rootl;
+int root1;
 int root2;
 double discriminant;
  cin >> a>>b>>c;
@@ -15,17 +15,17 @@ root1 = (-b + sqrt (discriminant))/ (2 * a);
 root2 = (-b - sqrt (discriminant))/ (2 * a);
 
 if (discriminant == 0){
-cout <‹ "The single root is "‹‹rooti‹‹endl;
+cout << "The single root is "<< root1‹‹endl;
 }
 
 else if(discriminant > 0){
 cout << "Two real roots\n": 
-   cout <<"-First root "<<root1 << mIn";
-cout <<"-Second root " <‹ root2 <‹ "\n";
+   cout <<"-First root "<<root1 << "\n" << endl;
+cout <<"-Second root " << root2 << "\n"<< endl;
 }
 
 else if (discriminant < 0){
-cout << "Complex roots\n";
+cout << "Complex roots\n"<< endl;
 
 }
 
